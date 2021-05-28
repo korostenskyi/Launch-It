c38996ac813cbb031ab9b426cdfe00c124dc8e90
Roman Korostenskyi <romankorostensky@gmail.com>
2021-05-28 14:57:49 +0300

    fix: internal dto

--- launchitandroid/src/main/java/io/korostenskyi/launchitandroid/MainActivity.kt
+++ launchitandroid/src/main/java/io/korostenskyi/launchitandroid/MainActivity.kt
@@ -3,14 +3,16 @@ package io.korostenskyi.launchitandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
-import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import io.korostenskyi.launchitandroid.ui.theme.LaunchItTheme
 import io.korostenskyi.shared.model.Post
@@ -34,14 +36,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PostCard(post: Post) {
     Card(
-        elevation = 6.dp
+        elevation = 12.dp,
+        modifier = Modifier.clickable {  }
     ) {
         Column {
-            Text(text = post.title)
+            Text(
+                text = post.title,
+                fontWeight = FontWeight.Bold
+            )
             Text(text = post.body)
         }
     }
-    Divider()
 }
 
 @Composable
--- shared/src/commonMain/kotlin/io/korostenskyi/shared/network/model/PostResponse.kt
+++ shared/src/commonMain/kotlin/io/korostenskyi/shared/network/model/PostResponse.kt
@@ -3,7 +3,7 @@ package io.korostenskyi.shared.network.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class PostResponse(
+data class PostResponse(
     val userId: Int,
     val id: Int,
     val title: String,
